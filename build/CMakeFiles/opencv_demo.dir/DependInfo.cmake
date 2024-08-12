862bdb38374b8745ffbbf4738f4028842f660a10
nb <820713556@qq.com>
2024-08-12 11:19:13 +0800

    add build

---
+++ build/CMakeFiles/opencv_demo.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gensong/tag/apriltag-master/example/opencv_demo.cc" "/home/gensong/tag/apriltag-master/build/CMakeFiles/opencv_demo.dir/example/opencv_demo.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "/apriltag"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/gensong/tag/apriltag-master/build/CMakeFiles/apriltag.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
